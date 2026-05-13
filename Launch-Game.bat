d78a90c9ddef1da8513473e50fc55807500ec941
jjiij <77879816+jjiij@users.noreply.github.com>
2026-05-13 23:38:22 +0200

    Remove legacy AutoPlay launcher files and rewrite README for WinForms

--- Launch-Game.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting LastChaos Launcher...
-start "" "LC.exe"
--- Update-Game.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting LastChaos Launcher updater...
-start "" "LC.exe"
